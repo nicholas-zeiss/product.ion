223dc39fd941ea54236b4fd0116d43e987c4ff46
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-10-17 04:31:25 -0400

    made expense actions async

--- app/client/actions/expenseActions.js
+++ app/client/actions/expenseActions.js
@@ -5,23 +5,54 @@
 **/
 
 
+import ApiCall from '../utils/serverCalls';
+
+
 export const clearExpenses = () => ({ type: 'CLEAR_EXPENSES' });
 
 
-export const createExpenses = expenses => ({ type: 'CREATE_EXPENSES', expenses });
+export const createExpenses = expenses => (
+	dispatch => ApiCall
+		.createExpenses(expenses)
+		.then(res => res, err => console.error(err))
+		.then(res => {
+			dispatch({ type: 'HYDRATE_EXPENSES', expenses: res.data });
+		})
+);
 
 
 export const dehydrateExpenses = ids => ({ type: 'DEHYDRATE_EXPENSES', ids });
 
 
-export const deleteExpense = id => ({ type: 'DELETE_EXPENSE', id });
+export const deleteExpense = id => (
+	dispatch => ApiCall
+		.deleteExpense(id)
+		.then(res => res, err => console.error(err))
+		.then(() => {
+			dispatch({ type: 'DEHYDRATE_EXPENSES', ids: [ id ] });
+		})
+);
 
 
-export const getExpenses = projIDs => ({ type: 'GET_EXPENSES', projIDs });
+export const getExpenses = projIDs => (
+	dispatch => ApiCall
+		.getExpenses(projIDs)
+		.then(res => res, err => console.error(err))
+		.then(res => {
+			dispatch({ type: 'HYDRATE_EXPENSES', expenses: res.data });
+		})
+);
 
 
 export const hydrateExpenses = expenses => ({ type: 'HYDRATE_EXPENSES', expenses });
 
 
-export const updateExpense = expense => ({ type: 'UPDATE_EXPENSE', expense });
+export const updateExpense = expense => (
+	dispatch => ApiCall
+		.updateExpense(expense)
+		.then(res => res, err => console.error(err))
+		.then(res => {
+			dispatch({ type: 'HYDRATE_EXPENSES', expenses: [ res.data ] });
+		})
+);
 
--- app/client/reducers/expenses.js
+++ app/client/reducers/expenses.js
@@ -12,27 +12,13 @@ import { store } from '../store';
 export const defaultExpensesState = { loaded: false };
 
 
-export default (state = {}, action) => {
+export default (state = defaultExpensesState, action) => {
 	
 	switch (action.type) {
 	
 
 		case 'CLEAR_EXPENSES': {
 			return defaultExpensesState;
-		}
-
-
-		case 'CREATE_EXPENSES': {
-			ApiCall
-				.createExpenses(action.expenses)
-				.then(res => {
-					store.dispatch({ type: 'HYDRATE_EXPENSES', expenses: res.data });
-				})
-				.catch(err => {
-					console.error(err);
-				});
-		
-			return state;
 		}
 
 
@@ -45,58 +31,16 @@ export default (state = {}, action) => {
 		}
 
 
-		case 'DELETE_EXPENSE': {
-			ApiCall
-				.deleteExpense(action.id)
-				.then(() => {
-					store.dispatch({ type: 'DEHYDRATE_EXPENSES', ids: [ action.id ] });
-				})
-				.catch(err => {
-					console.err(err);
-				});
-
-			return state;
-		}
-
-
-		case 'GET_EXPENSES': {
-			ApiCall
-				.getExpenses(action.projIDs)
-				.then(res => {
-					store.dispatch({ type: 'HYDRATE_EXPENSES', expenses: res.data });
-				})
-				.catch(err => {
-					console.error(err);
-				});
-		
-			return state;
-		}
-
-
 		case 'HYDRATE_EXPENSES': {
-			let newBudgets = Object.assign({}, state, { loaded: true });
+			let newExpenses = Object.assign({}, state, { loaded: true });
 
-			action.budgets.forEach(budget => {
-				let id = budget.id;
-				delete budget.id;
-				newBudgets[id] = budget;
+			action.expenses.forEach(expense => {
+				let id = expense.id;
+				delete expense.id;
+				newExpenses[id] = expense;
 			});
 
-			return newBudgets;
-		}
-
-
-		case 'UPDATE_EXPENSE': {
-			ApiCall
-				.updateExpense(action.expense)
-				.then(res => {
-					store.dispatch({ type: 'HYDRATE_EXPENSES', expenses: [ res.data ] });
-				})
-				.catch(err => {
-					console.error(err);
-				});
-		
-			return state;
+			return newExpenses;
 		}
 
 
